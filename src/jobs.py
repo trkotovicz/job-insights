375468f5d6ebe9b436dda76666366caed3b4d1cd
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-10-20 16:36:39 -0300

    feat: implementa a função read

--- src/jobs.py
+++ src/jobs.py
@@ -1,8 +1,13 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
+    with open(path, mode="r") as file:
+        file_reader = csv.DictReader(file)
+    return [row for row in file_reader]
+
     """Reads a file from a given path and returns its contents
 
     Parameters
@@ -15,4 +20,3 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
