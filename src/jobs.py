2bc273f45c878f0688e76a660742018cd93ef4d2
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-10-26 13:19:18 -0300

    tests: implementa testes da função count_ocurrences

--- src/jobs.py
+++ src/jobs.py
@@ -4,7 +4,7 @@ import csv
 
 @lru_cache
 def read(path):
-    with open(path) as file:
+    with open(path, 'r') as file:
         file_reader = csv.DictReader(file)
         list = [row for row in file_reader]
     return list
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,17 @@
-# from src.counter import count_ocurrences
+import json
+from unittest.mock import mock_open, patch
+
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    mock_text = {
+        "ocurrences": """This mock is here just to test the counter function.
+        So, I'm gonna throw some random words here:
+        python, Python, PYthON, pythoN,
+        javascript, Javascript, JavaScript, JAVASCRIPT"""
+    }
+
+    with patch("builtins.open", mock_open(read_data=json.dumps(mock_text))):
+        assert count_ocurrences("arquivo.json", "python") == 4
+        assert count_ocurrences("arquivo.json", "javascript") == 4
